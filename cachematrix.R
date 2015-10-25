c869ebcbd4cde53c375e170a4a169ced9240d471
zCoCo <connor.caxd@gmail.com>
2015-10-25 18:34:40 -0400

    Submission for Assignment 2
    Replaced all of the original code with my solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()){
+###############################################################################
+# Creates and Returns Matrix List Object which stores its own Inverse and 
+# contains Functions to Access that Data.
+# 
+# Args:
+#   M - The Data of the Matrix itself
+# 
+# Returns:
+#   A List containing Functions for Getting and Setting both the Core Matrix
+#   Data (in "M") and the Value of the Inverse (as if it were an object)
+###############################################################################
+    
+    i <- NULL;##The Inverse of this Matrix
+    
+    #Resets the Matrix Data:
+    setData <- function(M.new){
+        M <<- M.new;
+        i <<- NULL;##Data is New; Inverse is now Unknown
+    }
+    
+    #Returns the Stored Matrix Data:
+    getData <- function() M;
+    
+    
+    #Caches the Given Matrix Inverse:
+    setInverse <- function(inv) i <<- inv;
+    
+    #Returns the Inverse of the Matrix:
+    getInverse <- function() i;
+    
+    #Store Functions to a List and Return:
+    list(setData=setData, getData=getData,
+         setInverse=setInverse, getInverse=getInverse);
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mo){
+###############################################################################
+# Returns the Inverse for the Given Matrix List Object (as created in 
+# "makeCacheMatrix"). If the Matrix Object already contains a value for the
+# Inverse, that value is returned; however, if the Matrix Object's Inverse value
+# is "NULL", then the Inverse will be Calculated for the Matrix Object's Data,
+# Set as the Matrix Object's Inverse, and Returned.
+# 
+# Args:
+#   mo - Matrix Object to Compute the Inverse of. MUST be invertible.
+# 
+# Returns:
+#   The Inverse of the Data of the Given Matrix Object
+###############################################################################
+    
+    i <- mo$getInverse();##Inverse of the Matrix or "NULL" if not Computed
+    
+    #Check whether Inverse has Already been Solved and Cached:
+    if(!is.null(i)){
+        return(i);#Return Cached Inverse
+    }
+    
+    data <- mo$getData();##Core Matrix Data of Given Object
+    
+    #Compute Inverse:
+    i <- solve(mo);
+    #Cache Result:
+    mo$setInverse(i);
+    #Return Calculated Result:
+    i;
 }
